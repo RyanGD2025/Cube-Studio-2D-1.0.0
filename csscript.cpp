062f000ce62c2032547c18015c3209fd361c8b0a
RyanGD2025 <ryanbordez@gmail.com>
2025-11-01 22:51:29 -0300

    Create csscript.cpp

---
+++ csscript.cpp
@@ -0,0 +1,5 @@
+// csscript.cpp
+#include "csscript.h"
+bool CSScript::initialize() { return true; }
+void CSScript::tokenize(const std::string& code) { /* TODO */ }
+void CSScript::parse(const std::string& code) { /* TODO */ }
