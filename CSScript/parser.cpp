9cc08575b44677fdfb59a9143c162a63c37afbf6
RyanGD2025 <ryanbordez@gmail.com>
2025-11-01 22:55:06 -0300

    Create parser.cpp

---
+++ CSScript/parser.cpp
@@ -0,0 +1,9 @@
+// CSScript/parser.cpp
+#include "parser.h"
+
+Parser::Parser(const std::string& c) : code(c) {}
+
+ASTProgram* Parser::parseProgram() {
+    // placeholder
+    return new ASTProgram();
+}
