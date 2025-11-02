06606fa886ebd88b0ef1020501a246b1c8205262
RyanGD2025 <ryanbordez@gmail.com>
2025-11-01 22:49:50 -0300

    Create CubeStudio2D.cpp

---
+++ CubeStudio2D.cpp
@@ -0,0 +1,12 @@
+// CubeStudio2D.cpp
+#include "CubeStudio2D.h"
+bool CubeStudio2D::init() {
+    // inicialização da aplicação
+    return true;
+}
+void CubeStudio2D::render() {
+    // renderização básica (placeholder)
+}
+void CubeStudio2D::shutdown() {
+    // limpeza
+}
